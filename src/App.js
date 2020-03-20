89c19ed9bb3ca40a39991e22abe86e405efbc47c
RawBData <lunarmonky@yahoo.com>
2020-03-20 01:16:49 -0700

    finish modularizing mapdisplay

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React from "react";
 import { Map, Marker, Popup, TileLayer } from "react-leaflet";
 import { Icon } from "leaflet";
 import "./App.css";
+import Racks from "./maps/racks";
+import Traffic from "./maps/traffic";
 
 
 export const icon = new Icon({
@@ -20,13 +22,13 @@ class App extends React.Component{
   constructor(){
     super();
     this.state={
+      mainMapSelection:"racks",
       bikeRacks : [],
-      activeRack : null,
-      rackDisplay : false
+      accidentData: []
     }
 
-    this.activeRack = null; 
-    this.setActiveRack = null;
+    this.changeMap = this.changeMap.bind(this);
+
   }
 
   componentDidMount(){
@@ -40,68 +42,95 @@ class App extends React.Component{
     })
 
     console.log("state",this.state.bikeRacks);
+
   }
 
+  changeMap(newMap){
+    console.log("changing maps to ", newMap);
+      this.setState({
+        mainMapSelection : newMap
+      })
+  }
 
   render(){
+    let mapDisplay;
+    switch (this.state.mainMapSelection) {
+      case "racks":
+        mapDisplay = (<Racks racks={this.state.bikeRacks}/>)
+        break;
+      case "traffic":
+        console.log("chose traffic to display")
+        mapDisplay = (<Traffic racks={this.state.bikeRacks}/>)
+        break;
+    
+      default:
+        break;
+    }
+
+
     return (
       <div>
         <div>
-          <h1>
-            SFRider: Biking SF Timelapse Data
-          </h1>
+                <h1>
+                  SFRider: Biking SF Timelapse Data
+                </h1>
         </div>
-  
+
+        <div class="tabs-wrapper">
+          <div class="tabs">
+
+            <div class="tab">
+              <input type="radio" 
+                      name="css-tabs" 
+                      id="tab-1" 
+                      checked={this.state.mainMapSelection==="racks"} 
+                      class="tab-switch"
+                      onClick={()=>{this.changeMap("racks")}}
+              />
+              <label for="tab-1" class="tab-label">Racks</label>
+            </div>
+
+            <div class="tab">
+              <input type="radio" 
+                      name="css-tabs" 
+                      id="tab-2" 
+                      checked={this.state.mainMapSelection==="traffic"} 
+                      class="tab-switch"
+                      onClick={()=>{this.changeMap("traffic")}}
+              />
+              <label for="tab-2" class="tab-label">Traffic</label>
+            </div>
+
+            <div class="tab">
+              <input type="radio" 
+                      name="css-tabs" 
+                      id="tab-3" 
+                      checked={this.state.mainMapSelection==="accidents"} 
+                      class="tab-switch"
+                      onClick={()=>{this.changeMap("accidents")}}
+              />
+              <label for="tab-3" class="tab-label">Accidents</label>
+            </div>
+
+            <div class="tab">
+              <input type="radio" 
+                      name="css-tabs" 
+                      id="tab-4" 
+                      checked={this.state.mainMapSelection==="al"} 
+                      class="tab-switch"
+                      onClick={()=>{this.changeMap("all")}}
+              />
+              <label for="tab-4" class="tab-label">All</label>
+            </div>
+
+          </div> 
+        </div>
+
         <div>
-        <Map center={[37.773943, -122.449484]} zoom={13.4}>
-
-          <TileLayer
-            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-            attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-          />
-
-          {this.state.bikeRacks.map(rack => (
-            <Marker
-              key={rack.objectid}
-              position={[
-                rack.lat,
-                rack.lon
-              ]}
-              onClick={() => {
-                this.setActiverack = rack;
-                this.setState({
-                  activeRack: rack,
-                  rackDisplay: true
-                })
-              }}
-            />
-          ))}
-
-          {this.state.activeRack && (
-            <Popup
-              position={[
-                this.state.activeRack.lat,
-                this.state.activeRack.lon
-              ]}
-              onClose={() => {
-                this.setState({
-                  activeRack : null,
-                  rackDisplay : false
-                })
-              }}
-            >
-              <div>
-                <h2>Racks : {this.state.activeRack.racks}</h2>
-                <h2>Spaces : {this.state.activeRack.spaces}</h2>
-                <h2>Yr Installed : {this.state.activeRack.install_yr}</h2>
-
-              </div>
-            </Popup>
-          )}
-        </Map>
-  
+          {mapDisplay}
         </div>
-  
+
+        
       </div>
     );
 
--- src/maps/racks.jsx
+++ src/maps/racks.jsx
@@ -1,111 +1,63 @@
 import React from "react";
 import { Map, Marker, Popup, TileLayer } from "react-leaflet";
 import { Icon } from "leaflet";
-import "./App.css";
+import "../App.css";
 
 
-export const icon = new Icon({
-  iconUrl: "../public/bike_black.png",
-  iconSize: [25, 25]
-});
+export default function RacksMap({racks}) {
+  const [activeRack, setActiveRack] = React.useState(null);
 
-class App extends React.Component{
+  return (
+          <div>
+            
+      
+            <div>
+            <Map center={[37.773943, -122.449484]} zoom={13.4}>
+    
+              <TileLayer
+                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+              />
+    
+              {racks.map(rack => (
+                <Marker
+                  key={rack.objectid}
+                  position={[
+                    rack.lat,
+                    rack.lon
+                  ]}
+                  onClick={() => {
+                    {/* console.log("marker opened") */}
+                    setActiveRack(rack);
+                  }}
+                />
+              ))}
+    
+              {activeRack && (
+                <Popup
+                  position={[
+                    activeRack.lat,
+                    activeRack.lon
+                  ]}
+                  onClose={() => {
+                    {/* console.log("marker closed") */}
 
-  // SF Coordinates
-  // latitude:37.773943,
-  // longitude:-122.449484,
-
-  
-
-  constructor(){
-    super();
-    this.state={
-      bikeRacks : [],
-      activeRack : null,
-      rackDisplay : false
-    }
-
-    this.activeRack = null; 
-    this.setActiveRack = null;
-  }
-
-  componentDidMount(){
-    fetch("https://data.sfgov.org/resource/hn4j-6fx5.json")
-    .then(results=> {return results.json()})
-    .then(data=>{
-      console.log(data);
-      this.setState({
-        bikeRacks: data,
-      })
-    })
-
-    console.log("state",this.state.bikeRacks);
-  }
-
-
-  render(){
-    return (
-      <div>
-        <div>
-          <h1>
-            SFRider: Biking SF Timelapse Data
-          </h1>
-        </div>
-  
-        <div>
-        <Map center={[37.773943, -122.449484]} zoom={13.4}>
-
-          <TileLayer
-            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-            attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-          />
-
-          {this.state.bikeRacks.map(rack => (
-            <Marker
-              key={rack.objectid}
-              position={[
-                rack.lat,
-                rack.lon
-              ]}
-              onClick={() => {
-                this.setActiverack = rack;
-                this.setState({
-                  activeRack: rack,
-                  rackDisplay: true
-                })
-              }}
-            />
-          ))}
-
-          {this.state.activeRack && (
-            <Popup
-              position={[
-                this.state.activeRack.lat,
-                this.state.activeRack.lon
-              ]}
-              onClose={() => {
-                this.setState({
-                  activeRack : null,
-                  rackDisplay : false
-                })
-              }}
-            >
-              <div>
-                <h2>Racks : {this.state.activeRack.racks}</h2>
-                <h2>Spaces : {this.state.activeRack.spaces}</h2>
-                <h2>Yr Installed : {this.state.activeRack.install_yr}</h2>
-
-              </div>
-            </Popup>
-          )}
-        </Map>
-  
-        </div>
-  
-      </div>
-    );
-
-  }
-}
-
-export default App;
+                    setActiveRack(null);
+                  }}
+                >
+                  <div>
+                    <h2>Racks : {activeRack.racks}</h2>
+                    <h2>Spaces : {activeRack.spaces}</h2>
+                    <h2>Yr Installed : {activeRack.install_yr}</h2>
+    
+                  </div>
+                </Popup>
+              )}
+            </Map>
+      
+            </div>
+      
+          </div>
+        );
+                
+      }
