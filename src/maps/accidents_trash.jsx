332bb7d63a3f32b0affabf3f102a0c2c876bcf68
RawBData <lunarmonky@yahoo.com>
2020-03-28 13:27:06 -0700

    update racks

---
+++ src/maps/accidents_trash.jsx
@@ -0,0 +1,74 @@
+import React, { Component } from 'react'
+import {
+  Circle,
+  FeatureGroup,
+  LayerGroup,
+  LayersControl,
+  Map,
+  Marker,
+  Popup,
+  Rectangle,
+  TileLayer,
+} from "react-leaflet"
+const { BaseLayer, Overlay } = LayersControl;
+
+const center = [37.773943, -122.449484]
+const rectangle = [
+  [37.773943, -122.449484],
+  [37.773943, -122.49],
+]
+
+export default class LayersControlExample extends Component<{}> {
+  render() {
+    return (
+      <Map center={center} zoom={13}>
+        <LayersControl position="topright" collapsed={false}>
+          <BaseLayer name="Color">
+            <TileLayer
+              attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+              url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            />
+          </BaseLayer>
+          <BaseLayer checked name="Black and White">
+            <TileLayer
+              attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+              url="https://tiles.wmflabs.org/bw-mapnik/{z}/{x}/{y}.png"
+            />
+          </BaseLayer>
+          <Overlay name="Injury">
+            <Marker position={center}>
+              <Popup>
+                A pretty CSS3 popup. <br /> Easily customizable.
+              </Popup>
+            </Marker>
+          </Overlay>
+          <Overlay checked name="Fatal">
+            <LayerGroup>
+              <Circle center={center} fillColor="blue" radius={200} />
+              <Circle
+                center={center}
+                fillColor="red"
+                radius={100}
+                stroke={false}
+              />
+              <LayerGroup>
+                <Circle
+                  center={[center[0], center[1]-0.05]}
+                  color="green"
+                  fillColor="green"
+                  radius={100}
+                />
+              </LayerGroup>
+            </LayerGroup>
+          </Overlay>
+          <Overlay name="Path">
+            <FeatureGroup color="purple">
+              <Popup>Popup for path</Popup>
+              <Rectangle bounds={rectangle} />
+            </FeatureGroup>
+          </Overlay>
+        </LayersControl>
+      </Map>
+    )
+  }
+}
--- src/maps/racks.jsx
+++ src/maps/racks.jsx
@@ -1,27 +1,148 @@
 import React from "react";
-import { Map, Marker, Popup, TileLayer,  } from "react-leaflet";
+import { Map, Marker, Popup, TileLayer, FeatureGroup, LayerGroup, LayersControl } from "react-leaflet";
 import { Icon } from "leaflet";
 import "../App.css";
 import RacksLegend from './racksLegend';
+
+const { BaseLayer, Overlay } = LayersControl;
 
 const icon = new Icon({
   iconUrl: require("../images/lock.png"),
   iconSize: [30, 30]
 });
 
+const center = [37.773943, -122.449484]
+
 export default function RacksMap({racks}) {
   const [activeRack, setActiveRack] = React.useState(null);
   // let Deezracks = racks;
   return (
       <div>
-        
+
         <div> 
-          <Map center={[37.773943, -122.449484]} zoom={13.4}> 
-            <RacksLegend markers={racks.map(rack=>(rack))}/>
+        <Map center={[37.773943, -122.449484]} zoom={13.4}> 
             <TileLayer
               url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
               attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
             />    
+
+            <LayersControl position="topright" collapsed={false}>
+                
+                <BaseLayer name="2015-Present">
+                  <LayerGroup>
+                      {racks.filter(rk=>parseInt(rk.install_yr)>2014).map( rk => (
+                        <Marker
+                          key={rk.objectid}
+                          position={[
+                            rk.lat,
+                            rk.lon
+                          ]}
+                          onClick={() => {
+                            {/* console.log("marker opened") */}
+                            setActiveRack(rk);
+                          }}
+                          icon={icon}
+                        />
+                      ))}
+                  </LayerGroup>
+                </BaseLayer>
+
+                <BaseLayer name="2010-2014">
+                  <LayerGroup>
+                      {racks.filter(rk=>parseInt(rk.install_yr)>2009 && parseInt(rk.install_yr)<2015).map( rk => (
+                        <Marker
+                          key={rk.objectid}
+                          position={[
+                            rk.lat,
+                            rk.lon
+                          ]}
+                          onClick={() => {
+                            {/* console.log("marker opened") */}
+                            setActiveRack(rk);
+                          }}
+                          icon={icon}
+                        />
+                      ))}
+                  </LayerGroup>
+                </BaseLayer>
+
+                <BaseLayer checked name="2005-2009">
+                  <LayerGroup>
+                      {racks.filter(rk=>parseInt(rk.install_yr)>2004 && parseInt(rk.install_yr)<2010).map( rk => (
+                        <Marker
+                          key={rk.objectid}
+                          position={[
+                            rk.lat,
+                            rk.lon
+                          ]}
+                          onClick={() => {
+                            {/* console.log("marker opened") */}
+                            setActiveRack(rk);
+                          }}
+                          icon={icon}
+                        />
+                      ))}
+                  </LayerGroup>
+                </BaseLayer>
+
+                <BaseLayer name="Pre-2005">
+                  <LayerGroup>
+                      {racks.filter(rk=>parseInt(rk.install_yr)<2005).map( rk => (
+                        <Marker
+                          key={rk.objectid}
+                          position={[
+                            rk.lat,
+                            rk.lon
+                          ]}
+                          onClick={() => {
+                            {/* console.log("marker opened") */}
+                            setActiveRack(rk);
+                          }}
+                          icon={icon}
+                        />
+                      ))}
+                  </LayerGroup>
+                </BaseLayer>
+
+                <BaseLayer name="All">
+                  <LayerGroup>
+                      {racks.map( rk => (
+                        <Marker
+                          key={rk.objectid}
+                          position={[
+                            rk.lat,
+                            rk.lon
+                          ]}
+                          onClick={() => {
+                            {/* console.log("marker opened") */}
+                            setActiveRack(rk);
+                          }}
+                          icon={icon}
+                        />
+                      ))}
+                  </LayerGroup>
+                </BaseLayer>
+                
+            </LayersControl>
+
+
+            {activeRack && (
+                  <Popup
+                    position={[
+                      activeRack.lat,
+                      activeRack.lon
+                    ]}
+                    onClose={() => {
+                      {/* console.log("marker closed") */}
+
+                      setActiveRack(null);
+                    }}
+                  >
+                    <div>
+                      <h2>Spaces : {activeRack.spaces}</h2>
+                    </div>
+                  </Popup>
+                )}
           </Map>
         </div>
   
